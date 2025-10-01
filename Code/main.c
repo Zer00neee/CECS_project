484f03c408b85f251a091cf8e341888c61ee1989
Zer00ne <winnerfahsai@gmail.com>
2025-10-01 20:34:20 +0700

    edit main
    addtocsv 1.เช็คคะแนนว่า(1-5)ไหม (โครตงงมาติดแค่ตรง \n 😢) 2.มีเมนูให้เลือก y n r
    addE 1. ให้ใส่ข้อมูลลงไปแล้วไปทำในฟังชั่น addtocsvต่อ
    search มีเมนู y n r
    time 17:00 - 20:00 🥱

--- Code/main.c
+++ Code/main.c
@@ -1,73 +1,122 @@
 #include<stdio.h>
 #include<string.h>
 #define FILECSV "evaluations.csv"
-int choice;
 
-//.................................//
-  // สร้างStruct ขึ้นมาเก็บข้อมูลในcsv //
+void addtocsv(char name[], int rating, char date[], char feedback[]){
+       if(rating < 1 || rating > 5) {
+        printf("!!!rating (1-5)!!!!\n");
+        printf("Enter again y = yes / n = no / r = retun\n");
+        char type;
+        scanf(" %c", &type);
+        if(type == 'y'){
+            addEvaluation();
+        }
+        if(type == 'n'){
+            printf("Thank you\n");
+        }
+        if(type == 'r'){
+            start();
+        }
+        }
+    else{
+         int fileExists = 0;
+    FILE *fr = fopen("evaluation.csv", "r");
+    if(fr != NULL){
+        fileExists = 1;
+        fclose(fr);
+    }
+    FILE *fp = fopen("evaluation.csv", "a");
+    if(fp == NULL){
+        printf("Cannot open file!\n");
+    }
+    if(fileExists == 0){
+        fprintf(fp, "EvaluatorName,ServiceRating,EvaluatorDate,Feedback\n");
+    }
 
-struct Evaluation{
-    char name[100];
-    int rating;
-    char date[30];
-    char comment[500];
-};
-
-//.................................//
-       // สร้างFuntionกรอกข้อมูล//
-
-int addEvaluation(){
-    struct Evaluation e;
-    FILE *fp = fopen(FILECSV, "a");  // a คือเพิ่มข้อมูลต่อท้าย //
-    if (!fp) {                         
-        printf("Cannot open the file\n");
-}
-    fprintf(fp,"EvaluationNAME, ServiceRating, EvaluationDate, Comment\n");
+    fprintf(fp, "%s,%d,%s,%s\n", name, rating, date, feedback);
+    fclose(fp);
+    char choice;
+    printf("add more y = yes / n = no / r = retun: ");
+    scanf(" %c", &choice);
+    if(choice == 'y'){
+        addEvaluation();
+    }
+    if(choice == 'n'){
+        printf("Thank you\n");
+    }
+    if(choice == 'r'){
+        start();
+    }
+    }
    
-    printf("Enter evaluator's name: ");
-    scanf(" %s", &e.name);
-
-    printf("Enter score (1-5): ");
-    scanf("%d", &e.rating);
-
-    printf("Enter date (XXXX-XX-XX): ");
-    scanf(" %s", &e.date);
-
-    printf("Enter comment: ");
-    scanf(" %s", &e.comment);
-
-    fprintf(fp, "%s,%d,%s,%s\n", e.name, e.rating, e.date, e.comment); 
 }
-//.................................//
-       // สร้างFuntionอ่านข้อมูล//
 
-int readEvaluation(){
-    struct Evaluation e;
-    FILE *fp = fopen(FILECSV, "r");  // r คืออ่านอย่างเดียว //
-     if (!fp) {                         
-        printf("Cannot open the file\n");
+void addEvaluation() {
+    char name[50];
+    int rating;
+    char date[11];
+    char feedback[500];
+
+    printf("Enter name: ");
+    scanf("%49s", name);
+    printf("Enter score(1-5): ");
+    scanf("%d", &rating);
+    printf("EnterDate(DD-MM-YYYY):");
+    scanf("%10s", date);
+    printf("Enter feedblack: ");
+    scanf("%499s", feedback);
+    addtocsv(name, rating, date, feedback);
 }
-    char line[1000];
-    printf("Reading evaluation\n");
-    while (fgets(line, sizeof(line), fp)) {
-        struct Evaluation e;
-        sscanf(line, "%s,%d,%s,%s\n", e.name, e.rating, e.date, e.comment);
-        printf("Name %s rating %d date %s commet %s\n", e.name, e.rating, e.date, e.comment);
+
+void searchEvaluation() {
+    char searchName[50];
+    char line[600];
+    int found = 0; 
+    printf("Enter the name to search: ");
+    scanf("%49s", searchName);
+    FILE *fr = fopen("evaluation.csv", "r");
+    if(fr == NULL) {
+        printf("Cannot open file!\n");
+    }
+    fgets(line, sizeof(line), fr);
+    printf("Search Results:\n");
+    while(fgets(line, sizeof(line), fr) != NULL){
+        if(strstr(line, searchName) != NULL) {
+            printf("%s", line);
+            found = 1;
+        }
+    }
+    if(found != 1) {
+        printf("Not found %s\n", searchName);
+    }
+    fclose(fr);
+    char choice;
+    printf("search more y = yes / n = no / r = retun: ");
+    scanf(" %c", &choice);
+    if(choice == 'y'){
+        searchEvaluation();
+    }
+    if(choice == 'n'){
+        printf("Thank you\n");
+    }
+    if(choice == 'r'){
+        start();
+    }
 }
-fclose(fp);
+
+void start(){
+     int choice;
+    printf("Customer Service Evaluation Data Management System\n");
+    printf("1. Add evaluation\n");
+    printf("2. Search evaluation\n");
+    printf("Select menu: ");
+    scanf("%d", &choice);
+    switch(choice) {
+    case 1: addEvaluation(); break; 
+    case 2: searchEvaluation(); break;
+    }
 }
 
 int main() {
-    struct Evaluation e;   
-        printf("Customer Service Evaluation Data Management System\n");
-        printf("1. Add evaluation data\n");
-        printf("2. Read evaluation\n");
-        printf("Select menu: ");
-        scanf("%d", &choice);
-    switch (choice) {
-        case 1: addEvaluation(); break;
-        case 2: readEvaluation(); break;
-
-        
-}
+   start();
 }
